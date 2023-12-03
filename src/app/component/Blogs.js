3dfdc21b0e4151cc3c3aa940a646b72b8916d701
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-12-03 16:57:19 +0530

    added All the component Needed

---
+++ src/app/component/Blogs.js
@@ -0,0 +1,246 @@
+import React from "react";
+
+const Blogs = () => {
+  return (
+    <>
+      <div className="text-center max-w-xl mx-auto py-7">
+        <h1 className="text-6xl py-4 md:text-7xl font-bold mb-5 text-gray-600">
+          Milton blog
+        </h1>
+        <h3 className="text-xl mb-5 font-light">
+        All you need to start using our incredible tool correctly. And nothing else.
+        </h3>
+        <div className="text-center mb-10 py-4">
+          <span className="inline-block w-1 h-1 rounded-full bg-indigo-500 ml-1" />
+          <span className="inline-block w-3 h-1 rounded-full bg-indigo-500 ml-1" />
+          <span className="inline-block w-40 h-1 rounded-full bg-indigo-500" />
+          <span className="inline-block w-3 h-1 rounded-full bg-indigo-500 ml-1" />
+          <span className="inline-block w-1 h-1 rounded-full bg-indigo-500 ml-1" />
+        </div>
+      </div>
+
+      <div class="container mx-auto  px-4 lg:px-8  items-center ">
+        <div class="grid grid-cols-1 py-11 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="max-w-sm border">
+            <a href="#">
+              <img
+                className="rounded-xl"
+                src="https://framerusercontent.com/images/pDdypz9gmJH6vw6YOIlQL9oYMs.png?scale-down-to=1024"
+                alt
+              />
+            </a>
+            <div className="p-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                  Mastering Your Schedule: Top Time Management Techniques for
+                  Balancing Work and Life
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700">
+                Discover how to achieve harmony between your work and personal
+                life with these top time management techniques.
+              </p>
+              <a
+                href="#"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
+          <div className="max-w-sm border">
+            <a href="#">
+              <img
+                className="rounded-xl"
+                src="https://framerusercontent.com/images/9aMHBVjd140VZJzgdhbqZkuW4.png?scale-down-to=1024"
+                alt
+              />
+            </a>
+            <div className="p-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                  From Chaos to Clarity: Organizing Your Calendar for Enhanced
+                  Productivity and Focus
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700">
+                Discover how to transform your chaotic calendar into a
+                well-organized tool that boosts productivity and sharpens focus.
+              </p>
+              <a
+                href="#"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
+          <div className="max-w-sm border">
+            <a href="#">
+              <img
+                className="rounded-xl"
+                src="https://framerusercontent.com/images/ODCfJEiBLpq8z9C0bJW8I8pM76I.png?scale-down-to=1024"
+                alt
+              />
+            </a>
+            <div className="p-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                  The Power of Prioritization: Effective Strategies for Managing
+                  Your Time and Tasks
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700">
+                Discover how mastering the art of prioritization can transform
+                your productivity and help you achieve more in less time.
+              </p>
+              <a
+                href="#"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
+          <div className="max-w-sm border ">
+            <a href="#">
+              <img
+                className="rounded-xl"
+                src="https://img.freepik.com/free-vector/flat-spring-forward-fall-back-illustration_23-2149299330.jpg?w=1380&t=st=1701601808~exp=1701602408~hmac=d3b20cfe251be14095f9e4717d8e4fad4851db5bf9ea20a3d3f14010a77b0bc7"
+                alt
+              />
+            </a>
+            <div className="p-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                  Maximizing Your Minutes: Essential Tips for Streamlining Your
+                  Daily Routine and Calendar
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700">
+                Discover how to make the most of every minute with proven
+                strategies for optimizing your daily routine and calendar
+                management.
+              </p>
+              <a
+                href="#"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
+          <div className="max-w-sm border">
+            <a href="#">
+              <img
+                className="rounded-xl"
+                src="https://framerusercontent.com/images/9VwTuCQudXA6piwgktocQXqCMAc.png"
+                alt
+              />
+            </a>
+            <div className="p-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                  The Art of Saying No: How to Set Boundaries and Protect Your
+                  Time for What Matters Most
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700">
+                HDiscover the power of setting boundaries by learning the art of
+                saying no, enabling you to prioritize your time and focus on
+                what truly matters.
+              </p>
+              <a
+                href="#"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Blogs;
