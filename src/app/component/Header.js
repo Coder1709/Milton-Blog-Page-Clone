ded333f25b4b8229a02394537fa04f0adeb4211c
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-12-03 18:56:23 +0530

    Updated the testimony page

--- src/app/component/Header.js
+++ src/app/component/Header.js
@@ -80,7 +80,7 @@ export default function Header() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/testimony"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                 >
                   Testimonial
